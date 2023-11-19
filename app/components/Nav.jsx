aff056faffb44e566997c189486ebb3388764eda
zacharycallahan <zachcallahan90@gmail.com>
2023-11-19 00:57:26 -0500

    Add mute/unmute button

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,29 +3,31 @@ import Logo from "./Logo";
 
 const Nav = () => {
     return (
-        <nav className="bg-primary-gray py-4 md:py-6 top-0 w-full z-10 text-white shadow-md">
+        <nav className="bg-primary-gray py-4 top-0 w-full z-10 text-white shadow-md">
             {/* Container */}
-            <div className="container mx-auto flex justify-between items-center px-4 md:px-6">
+            <div className="container mx-auto flex justify-between items-center px-4">
 
                 {/* Logo and Brand Name */}
-                    <Link href={'/'} className="flex items-center">
-                        <Logo width={300} height={300} />
-                    </Link>
+                <Link className="hidden md:inline-block" href={'/'}>
+                    <div className="flex items-center cursor-pointer">
+                        <Logo width={300} height={300} className="w-12 h-12 md:w-16 md:h-16" />
+                    </div>
+                </Link>
 
                 {/* Navigation Items */}
-                <ul className="flex gap-4 md:gap-6 text-sm md:text-lg">
-                    <li className="font-medium hover:text-6sixshooter-blue hover:underline text-white transition-all duration-300 ease-in-out text-xl">
-                        <Link href={"https://www.tiktok.com/@the6ixshooter"}>
-                        TikTok
-                        </Link>    
+                <ul className="flex w-full justify-center gap-4 items-center text-sm md:gap-6 md:text-lg">
+                    <li className="font-medium hover:text-6sixshooter-blue hover:underline transition-all duration-300 ease-in-out">
+                        <Link href={"https://www.tiktok.com/@the6ixshooter"} className="text-xl">
+                            TikTok
+                        </Link>
                     </li>
-                    <li className="font-medium hover:text-6sixshooter-blue hover:underline text-white transition-all duration-300 ease-in-out text-xl">
-                        <Link href={"https://www.youtube.com/@The6ixShooter"}>
-                        Youtube
+                    <li className="font-medium hover:text-6sixshooter-blue hover:underline transition-all duration-300 ease-in-out">
+                        <Link href={"https://www.youtube.com/@The6ixShooter"} className="text-xl">
+                            Youtube
                         </Link>
                     </li>
                     <li className="font-medium">
-                        <Link href={"https://apps.apple.com/us/app/6ixshooter-academy-training/id1665280308"} className="px-3 py-2 bg-6sixshooter-blue text-white text-xl font-bold rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                        <Link href={"https://apps.apple.com/us/app/6ixshooter-academy-training/id1665280308"} className="block p-1 md:px-3 md:py-2 bg-6sixshooter-blue text-white font-bold rounded-sm md:rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300 text-sm md:text-xl">
                             Download the App
                         </Link>
                     </li>
--- app/components/sections/Home.jsx
+++ app/components/sections/Home.jsx
@@ -1,23 +1,38 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 
 const Home = () => {
+    const [muted, setMuted] = useState(true);
+
+    const toggleMute = () => {
+        setMuted(!muted);
+    }
     return (
-        <section className="snap-child ">
+        <section className="h-auto md:h-[820px]">
             <video
-                className=" w-full h-[820px] object-cover "
-                src="https://digitalassets.tesla.com/tesla-contents/video/upload/Homepage-Model-Y-Desktop-NA.mp4"
+                className="w-full h-auto md:h-[820px] object-cover"
+                src="/HomeVid.mp4"
                 autoPlay
+                preload="auto"
                 loop
-                muted={false}></video>
-            <div className="absolute bottom-40 w-full">
-                <div className=" container m-auto flex justify-between items-center">
-                    {/* TODO: make it shoot basketballs on the screen when hovered */}
-                    <Link href={"https://apps.apple.com/us/app/6ixshooter-academy-training/id1665280308"} className="px-7 py-4 bg-6sixshooter-blue text-white text-2xl font-bold rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                muted={muted}
+                disablePictureInPicture
+                />
+            <div className="absolute bottom-64 px-4 w-full flex justify-end">
+                <button onClick={toggleMute}>
+                    {!muted ? <FaVolumeUp className="border-4 rounded-full bg-black p-2" size={'64px'} color="white" /> : <FaVolumeMute className="border-4 rounded-full bg-black p-2" size={'64px'} color="white" />}
+                </button> 
+            </div>
+            <div className="hidden md:block relative md:bottom-28 lg:bottom w-full">
+                <div className="flex flex-col md:flex-row justify-between items-center px-4 w-full">
+                    <Link href={"https://apps.apple.com/us/app/6ixshooter-academy-training/id1665280308"} className="block w-fit p-2 lg:px-7 lg:py-4 bg-6sixshooter-blue text-white  text-xl lg:text-2xl font-bold rounded-lg shadow-lg hover:bg-blue-600 transition duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-300 mb-4 md:mb-0 text-center">
                         DOWNLOAD THE 6IXSHOOTER APP TODAY
                     </Link>
 
-                    <Image className=" bg-gray-200 p-2 rounded-md backdrop-blur-sm bg-opacity-10 transition duration-300 ease-in-out hover:scale-105 " width={300} height={50} alt="Featured on ESPN" src={"/ESPN_logo.png"} />
+                    <Image className="bg-gray-200 p-2 rounded-md backdrop-blur-sm bg-opacity-10 transition duration-300 ease-in-out hover:scale-105 " width={300} height={50} alt="Featured on ESPN" src={"/ESPN_logo.png"} />
                 </div>
             </div>
         </section>
